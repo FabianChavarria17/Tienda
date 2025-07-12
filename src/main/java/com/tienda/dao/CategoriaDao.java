484b46c30bff5acb3d1cb84d56450303b3d0d716
F_Chavarria <86330788+FabianChavarria17@users.noreply.github.com>
2025-07-12 14:57:33 -0600

    Semana 9

--- src/main/java/com/tienda/dao/CategoriaDao.java
+++ src/main/java/com/tienda/dao/CategoriaDao.java
@@ -2,12 +2,13 @@
 package com.tienda.dao;
 
 import com.tienda.domain.categoria;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface CategoriaDao extends JpaRepository<categoria, Long>{
     
-    
+    List<categoria> findByDescripcion(String descripcion);
     
     
     
--- src/main/java/com/tienda/service/impl/ProductoServiceImpl.java
+++ src/main/java/com/tienda/service/impl/ProductoServiceImpl.java
@@ -19,7 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class ProductoServiceImpl implements ProductoService {
 
-     @Autowired
+    @Autowired
     private ProductoDao productoDao;
 
     @Override
@@ -49,7 +49,26 @@ public class ProductoServiceImpl implements ProductoService {
     public void delete(Producto producto) {
         productoDao.deleteById(producto.getIdProducto());
     }
+
+    // Lista de productos con precio entre ordendados por descripción ConsultaAmpliada
+    @Override
+    @Transactional(readOnly = true)
+    public List<Producto> findByPrecioBetweenOrderByDescripcion(double precioInf, double precioSup) {
+        return productoDao.findByPrecioBetweenOrderByDescripcion(precioInf, precioSup);
+
+    }
     
+     @Override
+    @Transactional(readOnly=true)    
+    public List<Producto> metodoJPQL(double precioInf, double precioSup) {
+        return productoDao.metodoJPQL(precioInf, precioSup);
+    }
+
+    
+     @Override
+    @Transactional(readOnly=true)    
+    public List<Producto> metodoNativo(double precioInf, double precioSup) {
+        return productoDao.metodoNativo(precioInf, precioSup);
+    }
+
 }
-
-
